bb845cbd21068e24997bb822302c7eb265ab3279
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-01-29 04:21:42 +0000

    auto commit 01/29/20 04:21:41

--- h00/h00.cpp
+++ h00/h00.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here (Semester is OK)
+ *  @Mackenzie Cribbs Put your name here
+ *  @1/27/2020 Section 35993 Put the date here (Semester is OK)
  *  @file h00.cpp
  */
 #include <iostream>
@@ -16,14 +16,33 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    // Add your implementation comments here
+    // Input: weight of a box of cereal in ounces
+    // Output: weight of box in metric tons, boxes per ton.
+    // Given: metric ton is 35,273.92 ounces.
+    // Calculation: weight in tons->weight divided by metric tons per ounce
+    // Calculation: boxes-> 1 divided by weight in tons.
 
     // Write your code here
-    cout << STUDENT << " starter code." << endl;
-    cout << "[1, 1]" << endl;
+    cout << STUDENT << "-" << ASSIGNMENT << ": ";
+    cout << "Cereal Box Calculator" << endl;
+    cout << string(50, '-') << endl;
 
+    // Input
+    cout << "Enter ounces per box of cereal: "; // prompt
+    double ouncesPerBox; // store the input
+    cin >> ouncesPerBox; // read the input
+
+    // Processing section
+    const double OUNCES_PER_TON = 35273.92;
+    double weightInTons = ouncesPerBox / OUNCES_PER_TON;
+    double boxesPerTon = 1.0 / weightInTons;
+
+    // Output section
+    cout << "Weight in metric tons, boxes per ton: ["
+        << weightInTons << ", " << boxesPerTon << "]" << endl;
 
     return 0;
 }
+
 
 
