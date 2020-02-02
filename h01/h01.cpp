c5782ea6bdb784f2b9ca3ed2550b046fc4359f65
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-02 22:30:49 +0000

    auto commit 02/02/20 22:30:49

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Mackenzie Cribbs
+ *  @date 2/2/2020
  *  @file h01.cpp
  */
 #include <iostream>
@@ -8,7 +8,7 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "mcribbs";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 
@@ -18,9 +18,51 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    // Add your implementation comments here
+    cout << STUDENT << "-" << ASSIGNMENT << ": Time on my hands" << endl;
+    cout << "----------------------------------------------------" << endl;
+    cout << "Get time from input along with a duration" << endl;
+    cout << "The output will be the sum which is the time that follows the user's input" << endl;
+    cout << endl;
 
-    // Write your code here
+   // cout << "Time: " << "3:57"\n
+   // << "Duration: " << "1:05" << endl;
+
+    // Prompt and read the input
+    cout << "   Time: ";
+    int timeHours;
+    int timeMinutes;
+    int durationHours;
+    int durationMinutes;
+
+    cin >> timeHours;
+    cin.get(); // discards the colon
+    cin >> timeMinutes;
+
+    cout << "   Duration: ";
+    cin >> durationHours;
+    cin.get();
+    cin >> durationMinutes;
+
+    // Processing
+
+    int time = timeHours * 60 + timeMinutes;
+    int duration = durationHours * 60 + durationMinutes;
+    int after = time + duration;
+    int before = time - duration;
+    int sumHours = after / 60;
+    int sumMinutes = after % 60;
+    int diffHours = before / 60;
+    int diffMinutes = before % 60;
+
+    // Output
+    cout << endl;
+    cout << durationHours << ":" << durationMinutes << " hours after, and before, "
+    << timeHours << ":" << timeMinutes << " is [" << sumHours << ":" << sumMinutes
+    << ", " << diffHours << ":" << diffMinutes << "]" << endl;
+
+    cout << setfill('0');
+    cout << setw(2) << after / 60 << ":" << setw(2) << after % 60;
+
 
     return 0;
 }
