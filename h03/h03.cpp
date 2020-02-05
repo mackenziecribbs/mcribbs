4533fbbeeaff6309ea72a67424de5f29f4ef0fa9
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-05 06:12:48 +0000

    auto commit 02/05/20 06:12:48

--- h03/h03.cpp
+++ h03/h03.cpp
@@ -14,7 +14,6 @@ extern string ASSIGNMENT;
 
 // Function Prototypes
 void printTitle();
-cout << STUDENT << ASSIGNMENT << endl;
 string getInput();
 double letterToPoints(stringIn letterGrade);
 void printReport(double points);
@@ -39,3 +38,88 @@ int run()
 }
 
 // Implement your functions here
+void printTitle()
+{
+	cout << STUDENT << ASSIGNMENT << "-Grade Calculator" << endl;
+	string dash{20, '-'};
+	cout << dash << endl;
+}
+
+string getInput()
+{
+	string result;
+	cout << "Enter your letter grade: ";
+	getline(cin, result);
+	return result;
+}
+
+double letterToPoints(stringIn letterGrade)
+{
+	double doubleresult {0.0};
+	if (letterGrade == "A")
+	{
+		doubleresult = {4.0};
+	}
+	else if (letterGrade == "A-")
+	{
+		doubleresult = {3.7};
+	}
+	else if (letterGrade == "B+")
+	{
+		doubleresult = {3.4};
+	}
+	else if(letterGrade == "B")
+	{
+		doubleresult = {3.1};
+	}
+	else if(letterGrade == "B-")
+	{
+		doubleresult = {2.8};
+	}
+	else if(letterGrade == "C+")
+	{
+		doubleresult = {2.5};
+	}
+	else if(letterGrade == "C")
+	{
+		doubleresult = {2.2};
+	}
+	else if(letterGrade == "C-")
+	{
+		doubleresult = {1.9};
+	}
+	else if(letterGrade == "D+")
+	{
+		doubleresult = {1.6};
+	}
+	else if(letterGrade == "D")
+	{
+		doubleresult = {1.3};
+	}
+	else if(letterGrade == "D-")
+	{
+		doubleresult = {1.0};
+	}
+	else if(letterGrade == "A=" || "F+" || "F-")
+	{
+		doubleresult = {INVALID_COMBINATION};
+	}
+	else
+	{
+		doubleresult = {INVALID_INPUT};
+	}
+	return doubleresult;
+}
+
+void printReport(double points)
+{
+	if(points == INVALID_COMBINATION)
+	{
+		cout << "Invalid Input";
+	}
+	if(points == INVALID_COMBINATION)
+		cout << "Invalid combination";
+
+	cout << "Grade value is [" << points << "]" << endl;
+	//cout << "]";
+}
