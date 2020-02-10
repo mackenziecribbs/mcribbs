39a735fddff1a23468a9de8b11f5bc14b9dec4a4
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-10 06:51:46 +0000

    auto commit 02/10/20 06:51:46

--- h05/h05.cpp
+++ h05/h05.cpp
@@ -29,11 +29,11 @@ if (vowels.find(first) != string::npos )
 
 else if (masculine.find(country) != string::npos)
 {
-    prefix = " ";
+    prefix = "el";
 }
 else if(plain.find(country) != string::npos)
 {
-    prefix = "le";
+    prefix = "";
 }
 
 else if (country.substr(0,3) == islands || last == "es" || last == "is" || last == "os" || last == "as")
