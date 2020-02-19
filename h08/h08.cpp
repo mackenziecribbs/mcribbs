2fdbb80b334a668739730fe0076cde0b1a4da181
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-19 16:35:57 +0000

    auto commit 02/19/20 16:35:57

--- h08/h08.cpp
+++ h08/h08.cpp
@@ -16,12 +16,18 @@ string codeForDigit(int digit)
     string result = "";
     switch(digit)
     {
-        case 1: result = ":::||"; break;
-        case 2: result = "::|:|"; break;
-        case 3: result = "::||:"; break;
-        case 4: result = ":|::|"; break;
-        case 5: result = ":|:|:"; break;
-        case 6: result = ":||::"; break;
+        case 1: result = ":::||";
+        break;
+        case 2: result = "::|:|";
+        break;
+        case 3: result = "::||:";
+        break;
+        case 4: result = ":|::|";
+        break;
+        case 5: result = ":|:|:";
+        break;
+        case 6: result = ":||::";
+        break;
         case 7: result = "|:::|";
         break;
         case 8: result = "|::|:";
