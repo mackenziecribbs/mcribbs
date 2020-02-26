9a12f0052ed999bfe1b821c24700872aac04d4fe
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-26 07:38:53 +0000

    auto commit 02/26/20 07:38:53

--- h10/h10.cpp
+++ h10/h10.cpp
@@ -34,7 +34,8 @@ bool read(const string& str, string& name, bool nameb = false)
 }
 bool read(char& a, char sentinel)
 {
-    cout << a;
+    if(a == sentinel)
+        cout << a;
     return false;
 }
 
