a70a1c1df99e43a98b11f2b2c9af19a7177f1e0f
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-02-26 05:33:11 +0000

    auto commit 02/26/20 05:33:10

--- h09/h09.cpp
+++ h09/h09.cpp
@@ -1,20 +1,53 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Mackenzie Cribbs
+ *  @date Spring 2020
  *  @file h09.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 #include "h09.h"
 
 // Put your function implementation (definitions) in this file
+string digit(int n, const string& symbols)
+{
+    string result;
 
+    string sub1 = symbols.substr(0,1);
+    string sub2 = symbols.substr(1,1);
+    string sub3 = symbols.substr(2,1);
+    switch(n)
+    {
+        case 1: result = sub1;
+        break;
+        case 2: result = sub1 + sub1;
+        break;
+        case 3: result = sub1 + sub1 + sub1;
+        break;
+        case 4: result = sub1 + sub2;
+        break;
+        case 5: result = sub2;
+        break;
+        case 6: result = sub2 + sub1;
+        break;
+        case 7: result = sub2 + sub1 + sub1;
+        break;
+        case 8: result = sub2 + sub1 + sub1 + sub1;
+        break;
+        case 9: result = sub1 + sub3;
+        break;
+    }
+    return result;
+}
 
+string toRoman(int n)
+{
+    string result;
 
-
+    return result;
+}
 
 /////////// Student Testing ///////////////////////
 int run()
