1f7f291c6636ffd77377827ba2413989243c29d5
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-03-08 22:25:29 +0000

    auto commit 03/08/20 22:25:29

--- h15/h15.cpp
+++ h15/h15.cpp
@@ -1,12 +1,12 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Mackenzie Cribbs
+ *  @date Spring 2020
  *  @file h15.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 // NO OTHER CODE IN THIS FILE
 // PUT EVERYTHING IN THE HEADER FILE
