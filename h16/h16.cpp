39f667fabb87bcdc0d4afa50723cc9868d1cb084
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-03-11 05:04:18 +0000

    auto commit 03/11/20 05:04:18

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -19,7 +19,7 @@ void get(istream& in, Point& p)
 }
 void print(ostream& out, const Point& p)
 {
-    out << "(" << p.x << ", " << p.y << ")" << endl;
+    out << "(" << p.x << ", " << p.y << ")";
 }
 double distanceBetween(const Point& a, const Point& b)
 {
