86c7de4c3d71bc49e57be312507f13d456b0b9e7
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-03-11 05:00:32 +0000

    auto commit 03/11/20 05:00:32

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -11,7 +11,41 @@ string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 #include "h16.h"
 // Add your implementation here
+void get(istream& in, Point& p)
+{
+    char comma = ',';
+    in >> p.x >> comma >> p.y;
 
+}
+void print(ostream& out, const Point& p)
+{
+    out << "(" << p.x << ", " << p.y << ")" << endl;
+}
+double distanceBetween(const Point& a, const Point& b)
+{
+    double distance;
+    distance = sqrt(pow(b.y - a.y, 2) + pow(b.x-a.x, 2));
+
+    return distance;
+
+}
+Point midpoint(const Point& a, const Point& b)
+{
+
+    Point midPoint;
+    midPoint.x = (a.x+b.x) / 2;
+    midPoint.y = (a.y+b.y) / 2;
+    return midPoint;
+
+}
+double perimeter(const Triangle& t)
+{
+    double perimeter = distanceBetween(t.p1, t.p2);
+    perimeter += distanceBetween(t.p2, t.p3);
+    perimeter += distanceBetween(t.p3, t.p1);
+
+    return perimeter;
+}
 
 /////////////// STUDENT TESTING ////////////////////
 int run()
