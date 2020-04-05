7f2908e9993bfce8ea07b97acb06e0378d1ada4b
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-04-05 23:10:44 +0000

    auto commit 04/05/20 23:10:44

--- h20/h20.cpp
+++ h20/h20.cpp
@@ -14,7 +14,21 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 // Add your code here
 void greenScreen(unsigned char * const img, int width, int height)
 {
-
+unsigned char * p = img;
+int BPP = 4;
+const auto * end = img + width * height * BPP;
+while (p != end)
+{
+    unsigned char& red = *p;
+    unsigned char& green = *(p + 1);
+    unsigned char& blue = *(p + 2);
+    unsigned char& alpha = *(p + 3);
+    if (green > 2 * max(red, blue))
+    {
+        red = green = blue = alpha = 0;
+    }
+    p += 4;
+    }
 }
 
 
