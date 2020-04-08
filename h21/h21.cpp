c86fffb4160e6f45c59cc803e6be7bb7ffc409aa
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-04-08 02:16:40 +0000

    auto commit 04/08/20 02:16:39

--- h21/h21.cpp
+++ h21/h21.cpp
@@ -1,18 +1,37 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Mackenzie Cribbs
+ *  @date Spring 2020
  *  @file h21.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 #include "h21.h"
 
 // Add your code here
-
+const int BPP = 4;
+void composite (unsigned char * const bg, unsigned char * const fg, int width, int height)
+{
+    greenScreen(fg, width, height);
+    unsigned char * dest = fg;
+    unsigned char * src = bg;
+    unsigned char * end = dest + width * height * BPP;
+    while (dest != end)
+    {
+        if (*(dest + 3) == 0)
+        {
+            *dest = *src;
+            *(dest + 1) = *(src + 1);
+            *(dest + 2) = *(src + 2);
+            *(dest + 3) = *(src + 3);
+        }
+        src += 4;
+        dest += 4;
+    }
+}
 
 
 
