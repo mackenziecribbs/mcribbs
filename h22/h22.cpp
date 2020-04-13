3080ddbf101bc0e48f053037a309f5148c3891a4
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-04-13 03:49:02 +0000

    auto commit 04/13/20 03:49:02

--- h22/h22.cpp
+++ h22/h22.cpp
@@ -49,6 +49,48 @@ void flip(UC* const img, int width, int height, Direction dir)
 }
 void mirror(UC* const img, int width, int height, Direction dir)
 {
+     if (dir == Direction::LtoR || dir == Direction::RtoL)
+    {
+        for (int row = 0; row < height; ++row)
+        {
+            Pixel * front = reinterpret_cast<Pixel*>(img) + row * width; // allows the img to be a different kind of pointer
+            Pixel * back = front + width - 1; // last pixel
+            while (front < back)
+            {
+                if (dir == Direction::RtoL)
+                {
+                    *front = *back;
+                }
+                else
+                {
+                    *back = *front;
+                }
+                front++;
+                back--;
+            }
+        }
+    }
+    else // top to bottom or vice-versa
+    {
+        for (int col = 0; col < width; ++col)
+        {
+            Pixel * top = reinterpret_cast<Pixel*>(img) + col;
+            Pixel * bottom = top + width * (height - 1);
+            while (top < bottom)
+            {
+                if (dir == Direction::TtoB)
+                {
+                    *bottom = *top;
+                }
+                else
+                {
+                    *top = *bottom;
+                }
+                top += width;
+                bottom -= width;
+            }
+        }
+    }
 
 }
 
