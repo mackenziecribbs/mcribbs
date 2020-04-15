5fa7f6ceb34f00c930540e8481d514bdd9024450
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-04-15 02:09:03 +0000

    auto commit 04/15/20 02:09:02

--- h24/h24.cpp
+++ h24/h24.cpp
@@ -8,12 +8,51 @@
 // Other headers if necessary
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 #include "h24.h"
 
 // Add your code here
+bool sameSet(const int *aBeg, const int *aEnd, const int *bBeg, const int *bEnd)
+{
+    for (auto p = aBeg; p != aEnd; p++)
+    {
+        bool found = false;
+        for (auto p2 = bBeg; !found && p2 != bEnd; p2++)
+        {
+            if (*p2 == *p)
+            {
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            return false;
+        }
+    }
+       for (auto p = bBeg; p != bEnd; p++)
+    {
+        bool found = false;
+        for (auto p2 = aBeg; !found && p2 != aEnd; p2++)
+        {
+            if (*p2 == *p)
+            {
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            return false;
+        }
+    }
 
+        return true;
+}
+
+void copyEvens(const int a[], size_t aSize, int b[], size_t& bSize)
+{
+
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
