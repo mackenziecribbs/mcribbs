0ea2f993f27d83f1bd5641217ca26ea65a6cda39
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-04-22 15:42:02 +0000

    auto commit 04/22/20 15:42:00

--- h27/h27.cpp
+++ h27/h27.cpp
@@ -1,17 +1,35 @@
 /**
- *  @author Put your name here
+ *  @author Mackenzie Cribbs
  *  @date Put the date here
  *  @file h27.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 #include "h27.h"
 
 // Implement your class here
+Bug::Bug(int startPos) // construct Bug at starting position
+{
+    position_ = startPos;
+    direction_ = 1;
+}
 
+void  Bug::move() // move bug one unit in direction
+{
+    position_ += direction_;
+}
+
+void  Bug::turn()           // Change the direction from left to right or vice-versa
+{
+    direction_ *= -1;
+}
+int  Bug::position() const   // Return the position
+{
+    return position_;
+}
 
 
 
