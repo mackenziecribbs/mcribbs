fe4166cd7d68d1d5b513aff5a177643ec5d1b7fd
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-05-04 04:30:21 +0000

    auto commit 05/04/20 04:30:21

--- h31/h31.cpp
+++ h31/h31.cpp
@@ -1,5 +1,5 @@
 /**
- *  @author Put your name here
+ *  @author Mackenzie Cribbs
  *  @date Put the date here
  *  @file h31.cpp
  */
@@ -7,12 +7,37 @@
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 
 #include "h31.h"
 
 // Add your code here
+/**
+     * Rotates an image in both the dx and dy directions.
+     * Pixels wrap around as required.
+     * @param dx the amount to shift in the x dimension.
+     * @param dy the amount to shift in the y dimension
+     * Assume 4 bits per pixel
+     */
+void Image::translate(int dx, int dy)
+{
+
+}
+
+    /**
+     * Overloaded subscript operators.
+     * @param idx index from 0 < size
+     * @throws out_of_range if invalid index
+     */
+const Pixel& Image::operator[](unsigned idx) const
+{
+    return m_pixels.at(idx);
+}
+Pixel& Image::operator[](unsigned idx)
+{
+    return m_pixels.at(idx);
+}
 
 
 
