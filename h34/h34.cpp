e85920741d3f35bfc3c54b524f014def421de2f2
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-05-11 16:25:12 +0000

    auto commit 05/11/20 16:25:12

--- h34/h34.cpp
+++ h34/h34.cpp
@@ -17,8 +17,8 @@ string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
     Circle::Circle(double radius, double x, double y)
         : Point(x, y), m_radius(radius) {}
     double Circle::getRadius() const { return m_radius; }
-    double Circle::getArea() const { return 0; }
-    double Circle::getCircumference() const { return 0; }
+    double Circle::getArea() const { return PI * m_radius * m_radius; }
+    double Circle::getCircumference() const { return 2 * PI * m_radius; }
     std::string Circle::toString(int decimals) const
     {
         return "not implemented";
@@ -27,7 +27,7 @@ string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
     Cylinder::Cylinder(double height, double radius, double x, double y)
     : Circle(radius, x, y), m_height(height) {}
     double Cylinder::getHeight() const { return m_height; }
-    double Cylinder::getVolume() const { return 0; }
+    double Cylinder::getVolume() const { return Circle::getArea() * m_height; }
     double Cylinder::getArea() const { return 0; }
     std::string Cylinder::toString(int decimals) const
     {
