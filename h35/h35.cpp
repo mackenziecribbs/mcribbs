540cde068a5e3ff31576734d0e860b1d5748f401
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-05-13 03:15:48 +0000

    auto commit 05/13/20 03:15:48

--- h35/h35.cpp
+++ h35/h35.cpp
@@ -22,12 +22,26 @@ double Worker::getRate() const { return rate; }
 
 	HourlyWorker::HourlyWorker(const std::string& name, double rate)
 	    : Worker(name, rate) {}
-	std::string HourlyWorker::payInfo(int hours) const { return ""; }
+	std::string HourlyWorker::payInfo(int hours) const
+	{
+	    double pay = 0;
+	    if (hours > 40) pay = getRate() * 40 + (hours - 40) * getRate() * 1.5;
+	    else pay = getRate() * hours;
+
+	    ostringstream out;
+	    out << fixed << setprecision(2);
+	    out << getName() << "(Hourly, $ " << getRate() << ") worked "
+	        << hours << " hours. Pay : $ " << pay;
+	    return "";
+	}
 
 
     SalariedWorker::SalariedWorker(const std::string& name, double rate)
         : Worker(name, rate) {}
-	std::string SalariedWorker::payInfo(int hours) const { return ""; }
+	std::string SalariedWorker::payInfo(int hours) const
+	{
+	    return "";
+	}
 
 
 /////////////// Student Tests ////////////////////////////
