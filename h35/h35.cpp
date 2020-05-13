f14959c3f3a58bc418d14fb89dd81c59c57a7f59
Mackenzie Cribbs <mcribbs@student.cccd.edu>
2020-05-13 03:09:29 +0000

    auto commit 05/13/20 03:09:26

--- h35/h35.cpp
+++ h35/h35.cpp
@@ -1,6 +1,6 @@
 /**
     @file h35.cpp
-    @author your name here
+    @author Mackenzie Cribbs
     @version what day and meeting time
 */
 #include <string>
@@ -11,11 +11,23 @@ using namespace std;
 
 #include "h35.h"
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mcribbs"; // Add your Canvas/occ-email ID
 
 // Add your implementation here
 
+Worker::Worker(const std::string& name, double rate)
+    : name(name), rate(rate) {}
+std::string Worker::getName() const { return name; }
+double Worker::getRate() const { return rate; }
 
+	HourlyWorker::HourlyWorker(const std::string& name, double rate)
+	    : Worker(name, rate) {}
+	std::string HourlyWorker::payInfo(int hours) const { return 0; }
+
+
+    SalariedWorker::SalariedWorker(const std::string& name, double rate)
+        : Worker(name, rate) {}
+	std::string SalariedWorker::payInfo(int hours) const { return 0; }
 
 
 /////////////// Student Tests ////////////////////////////
